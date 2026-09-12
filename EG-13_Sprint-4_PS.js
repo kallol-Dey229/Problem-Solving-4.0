b563ea8569cda9fa1b2b02891fc2dab7f7da2e51
Kallol Dey <kalloldey067@gmail.com>
2026-09-12 16:19:58 +0600

    problem 8 solved

--- EG-13_Sprint-4_PS.js
+++ EG-13_Sprint-4_PS.js
@@ -243,3 +243,36 @@ var removeNthFromEnd = function(head, n) {
 
 
 // console.log(removeNthFromEnd([1, 2, 3, 4, 5], n = 2))
+
+
+
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+
+    let start = -1;
+    let end = -1;
+
+    for (let i = 0; i < nums.length; i++) {
+
+        if (nums[i] === target) {
+
+            if (start === -1) {
+                start = i;
+            }
+
+            end = i;
+        }
+    }
+
+    return [start, end];
+};
+
+
+
+// console.log(searchRange([5, 7, 7, 8, 8, 10], target = 8))

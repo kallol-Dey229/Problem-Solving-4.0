7cd3aca37041bcdcbb8d7e32aba839d59f4fdaf0
Kallol Dey <kalloldey067@gmail.com>
2026-09-12 13:34:29 +0600

    problem 1 solved

--- EG-13_Sprint-4_PS.js
+++ EG-13_Sprint-4_PS.js
@@ -1,1 +1,29 @@
-//
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function (s, t) {
+
+    let map1 = {};
+    let map2 = {};
+
+    for (let i = 0; i < s.length; i++) {
+
+        if (map1[s[i]] && map1[s[i]] !== t[i]) {
+            return false;
+        }
+
+        if (map2[t[i]] && map2[t[i]] !== s[i]) {
+            return false;
+        }
+
+        map1[s[i]] = t[i];
+        map2[t[i]] = s[i];
+    }
+
+    return true;
+};
+
+
+// console.log(isIsomorphic("egg", "add"))
